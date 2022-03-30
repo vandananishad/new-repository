2374a663b33e2ab795128a86570d6dbeedf8f0a9
vandananishad <vandu.bind@gmail.com>
2022-03-30 11:15:49 +0530

    first commit by x

---
+++ Demo/src/Test/Test1.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=10;
+		b=20;
+		c=a+b;
+		System.out.println("additio of two number="+c);
+		
+
+	}
+
+}
